14269d0ff2bb467c6945808e5b8dc1474390cf25
Enighander <ringgo.enighander@gmail.com>
2023-11-28 12:04:52 +0700

    added vercel.json

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const {Pool} = require('pg')
 const pool = new Pool({
-    host: process.env.localhost,
+    host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
