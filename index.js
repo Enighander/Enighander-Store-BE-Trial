634a5d8fe11b27433ce368fdc83c2f56ede866cb
Enighander <ringgo.enighander@gmail.com>
2023-11-28 14:35:31 +0700

    fixing cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT;
 
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'https://enighander-store-fe-trial.vercel.app' }));
 app.options('*', cors());
 app.use(helmet({
       crossOriginResourcePolicy: false,
